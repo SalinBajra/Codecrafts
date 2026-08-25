ad2b223e716c01031d4312c72dc34ad3cf645ba1
SalinBajra <salinmanbajracharya@gmail.com>
2026-08-25 09:28:28 -0500

    Support SEO file types in local preview

--- local-preview.ps1
+++ local-preview.ps1
@@ -1,6 +1,6 @@
 $ErrorActionPreference = 'Stop'
 $siteRoot = (Resolve-Path $PSScriptRoot).Path
-$types = @{'.html'='text/html; charset=utf-8';'.css'='text/css; charset=utf-8';'.js'='application/javascript; charset=utf-8';'.json'='application/json; charset=utf-8';'.xml'='application/xml; charset=utf-8';'.txt'='text/plain; charset=utf-8';'.png'='image/png';'.jpg'='image/jpeg';'.jpeg'='image/jpeg';'.svg'='image/svg+xml';'.ico'='image/x-icon';'.webp'='image/webp'}
+$types = @{'.html'='text/html; charset=utf-8';'.css'='text/css; charset=utf-8';'.js'='application/javascript; charset=utf-8';'.json'='application/json; charset=utf-8';'.webmanifest'='application/manifest+json; charset=utf-8';'.xml'='application/xml; charset=utf-8';'.txt'='text/plain; charset=utf-8';'.png'='image/png';'.jpg'='image/jpeg';'.jpeg'='image/jpeg';'.svg'='image/svg+xml';'.ico'='image/x-icon';'.webp'='image/webp'}
 $server = [Net.Sockets.TcpListener]::new([Net.IPAddress]::Loopback,4174)
 $server.Start()
 Write-Host 'CodeCrafts v2 preview running at http://localhost:4174'
